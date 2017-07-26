c9d11c074db6f2c1b9c538a576c275777b095763
Dhairya Chandra <dhairyachandra@outlook.com>
2017-07-26 23:55:55 +0530

    Update selectionsort.c

--- selectionsort.c
+++ selectionsort.c
@@ -1,37 +1,34 @@
-//Selection Sort
+#include <stdio.h>
+#include <stdlib.h>
 
-#include<stdio.h>
-#include<conio.h>
-#define max 200
-void selection_sort(int a[],int n)
- {
-   int i,j,temp=0;
-     for(i=0;i<n-1;i++)
-        {
-           for(j=i+1;j<n;j++)
-             {
-                 if(a[i]>a[j])
-                  {
-                   temp=a[i];
-                   a[i]=a[j];
-                   a[j]=temp;
-                  }
-             }
-        }
-    for(i=0;i<n;i++)
-        printf(" %d",a[i]);
-
- }
+void sort(int a[100], int n)
+{
+  int i,j,min,temp;
+  for (i=0;i<n;i++)
+  {
+    min = a[i];
+    for(j=i+1;j<n;j++)
+    {
+      if(a[j] < a[i])
+      {
+        temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+      }
+    }
+  }
+}
 int main()
 {
-
-     int n,a[max],i,j;
-     scanf("%d",&n);
-       for(i=0;i<n;i++)
-        {
-           scanf("%d",&a[i]);
-        }
-      selection_sort(a,n);
-    getch();
-    return 0;
+  int a[100],i,j,n;
+  scanf("%d",&n);
+  for (i=0;i<n;i++)
+  {
+      scanf("%d",&a[i]);
+  }
+  sort(a,n);
+  for(i=0;i<n;i++)
+  {
+      printf("%d ",a[i]);
+  }
 }
